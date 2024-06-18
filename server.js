87cc4770b758dde36147075e20b5f0546c481186
Uthai7472 <uthai.khantamongkhon.a7p@ap.denso.com>
2024-06-18 21:14:07 +0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
+const port = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port });
 
 wss.on('connection', function connection(ws) {
   console.log('Client connected');
